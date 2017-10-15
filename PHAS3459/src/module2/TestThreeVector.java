2138feca5a7050ef4a2d8f33bb83b0d433028afe
James Williams <zcapjwi@ucl.ac.uk>
2017-10-15 19:24:00 +0100

    defined ThreeVector Class

---
+++ PHAS3459/src/module2/TestThreeVector.java
@@ -0,0 +1,18 @@
+package module2;
+
+public class TestThreeVector {
+  
+
+
+  public static void main(String[] args) {
+  
+  ThreeVector v1 = new ThreeVector(3, 5, 2);
+v1.add(v1);
+  
+  
+  
+  
+  
+  }
+
+}
---
+++ PHAS3459/src/module2/ThreeVector.java
@@ -0,0 +1,28 @@
+package module2;
+
+public class ThreeVector {
+  
+  double x;
+  double y;
+  double z;
+  
+  public ThreeVector(double a, double b, double c) {
+    x = a;
+    y = b;
+    z = c;
+  }
+
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+  
+  
+
+
