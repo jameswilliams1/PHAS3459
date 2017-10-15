4fbf496eae0b206ac7f8cfe345e6142e8c3904d8
James Williams <zcapjwi@ucl.ac.uk>
2017-10-15 23:38:47 +0100

    working unitvector method

--- PHAS3459/src/module2/TestThreeVector.java
+++ PHAS3459/src/module2/TestThreeVector.java
@@ -7,9 +7,14 @@ public class TestThreeVector {
   public static void main(String[] args) {
   
   ThreeVector v1 = new ThreeVector(3, 5, 2);
-v1.add(v1);
-  
-  
+  ThreeVector v2 = new ThreeVector(2, 4, 1);
+  ThreeVector v3 = new ThreeVector(0, 0, 0);
+  double u12 = v1.unitVector().x;
+  double v12 = v1.unitVector().y;
+  double w12 = v1.unitVector().z;
+  System.out.println(u12);
+  System.out.println(v12);
+  System.out.println(w12);
   
   
   
--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -6,13 +6,48 @@ public class ThreeVector {
   double y;
   double z;
   
+  
+  
   public ThreeVector(double a, double b, double c) {
-    x = a;
-    y = b;
-    z = c;
+    this.x = a;
+    this.y = b;
+    this.z = c;
   }
+  
+  public double magnitude() {
+    return Math.sqrt(x*x+y*y+z*z); //magnitude = (a.a + b.b + c.c)^0.5
+  }
+  
 
+
+  public ThreeVector unitVector() {
+    
+    double i = x/magnitude();
+    double j = y/magnitude();
+    double k = z/magnitude();
+    return new ThreeVector(i,j,k);
+    
   }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   
   
