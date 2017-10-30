b89c66c66bc95cef1283410dd8449e3dba1fe4ca
James Williams <zcapjwi@ucl.ac.uk>
2017-10-30 11:48:51 +0000

    all working

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -6,27 +6,28 @@ import java.util.Scanner;
 
 public class NumericalReader {
   /*
-   * Defines functions for reading and processing numerical files
+   * Defines functions for reading and processing numerical files on the web
    */
 
-  private double minValue;
-  private double maxValue;
-  private int nValues;
-  private double sumOfValues;
+  private double minValue = 999999999999999.0;
+  private double maxValue = -999999999999999.0;
+  private int nValues = 0;
+  private double sumOfValues = 0.0;
   public String fileLoc = "";
 
   public static String getStringFromKeyboard() throws IOException {
-    // Prompts user to enter filepath & returns keyboard input through BufferedReader
-    System.out.println("Enter save location:");
+    // Prompts user to enter filepath & returns keyboard input through
+    // BufferedReader
+    System.out.println("Enter save directory:");
     InputStreamReader is = new InputStreamReader(System.in);
     BufferedReader br = new BufferedReader(is);
     String s = br.readLine();
-    if(s.length()!=0) {
+    if (s.length() != 0) {
       return s;
-    }
-    else {
+    } else {
       System.out.println("User home directory has been selected.");
-      return System.getProperty("user.home"); //Uses home directory if no input
+      System.out.println();
+      return System.getProperty("user.home"); // Uses home directory if no input
     }
   }
 
@@ -38,39 +39,52 @@ public class NumericalReader {
     return new BufferedReader(isr);
   }
 
-  public void analysisStart(String filePath) throws IOException {
+  public void analysisStart(String dataFile) throws IOException {
     // Creates file in specified path
-    File outputfile = new File(filePath);
+    File outputfile = new File(dataFile);
     FileWriter fw = new FileWriter(outputfile);
     fw.close();
   }
-  
+
   public void analyseData(String line) throws IOException {
     Scanner s = new Scanner(line);
     BufferedWriter bw = new BufferedWriter(new FileWriter(this.fileLoc, true));
-    // 
+
     while (s.hasNext()) {
-      if(s.hasNextDouble()) {
+
+      if (s.hasNextDouble()) {
         Double number = s.nextDouble();
         System.out.println(number);
         String numberAsString = Double.toString(number);
         bw.write(numberAsString);
         bw.newLine();
-        
-        this.minValue = number;
-        
-        this.minValue = number;
+
+        this.nValues++;
+        this.sumOfValues += number;
+
+        if (this.minValue > number) {
+          this.minValue = number;
+        } else if (this.maxValue < number) {
+          this.maxValue = number;
+        }
       }
-      else if(s.hasNextInt()) {
+
+      else if (s.hasNextInt()) {
         System.out.println(s.nextInt());
-        //bw.write(s.nextInt());
-      }
-      else {
+      } else {
         s.nextLine();
       }
+
     }
     s.close();
     bw.close();
+  }
+
+  public void analysisEnd() {
+    System.out.println("The minimum value was: " + this.minValue);
+    System.out.println("The maximum value was: " + this.maxValue);
+    System.out.println("The average value was: " + (this.sumOfValues / this.nValues));
+    System.out.println("The total was: " + this.sumOfValues);
   }
 
   public static void main(String[] args) {
@@ -82,43 +96,43 @@ public class NumericalReader {
 
     try {
       saveDir = NumericalReader.getStringFromKeyboard();
-    } 
-    catch (java.io.IOException e) {
+    } catch (java.io.IOException e) {
       System.out.println(e);
     }
     try {
       nr1.fileLoc = (saveDir + File.separator + "numbers1.txt");
       System.out.println("Saving to " + nr1.fileLoc);
       nr1.analysisStart(nr1.fileLoc);
-      
+
       // Creates BufferedReader object from a web page
-      BufferedReader webBuffer1 = nr1.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
+      BufferedReader webBuffer1 = nr1
+          .brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
       // Prints each number until readLine returns a null (empty) line
       while ((line = webBuffer1.readLine()) != null) {
         nr1.analyseData(line);
       }
-    }
-    catch (java.io.IOException e) {
+    } catch (java.io.IOException e) {
       System.out.println(e);
     }
+    nr1.analysisEnd(); // Print min, max, avarage, total
+    System.out.println();
+
     try {
       nr2.fileLoc = (saveDir + File.separator + "numbers2.txt");
       System.out.println("Saving to " + nr2.fileLoc);
       nr2.analysisStart(nr2.fileLoc);
-      
+
       // Creates BufferedReader object from a web page
-      BufferedReader webBuffer2 = nr2.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt");
+      BufferedReader webBuffer2 = nr2
+          .brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt");
       // Prints each number until readLine returns a null (empty) line
       while ((line = webBuffer2.readLine()) != null) {
         nr2.analyseData(line);
       }
-    }
-    catch (java.io.IOException e) {
+    } catch (java.io.IOException e) {
       System.out.println(e);
     }
+    nr2.analysisEnd(); // Print min, max, avarage, total
 
-    
-    
-   
   }
 }
