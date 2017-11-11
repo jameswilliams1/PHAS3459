4d8171c4d19774c9b3c3d69144a47ac60fa9ef29
James Williams <zcapjwi@ucl.ac.uk>
2017-11-11 15:23:28 +0000

    working matrix constructor and tostring

--- PHAS3459/src/module5/SquareMatrix.java
+++ PHAS3459/src/module5/SquareMatrix.java
@@ -1,5 +1,54 @@
 package module5;
 
-public class SquareMatrix {
+import java.lang.reflect.Array;
 
-}
+public class SquareMatrix {
+  /*
+   * Represents a square matrix using a 2-D array
+   */
+  
+  private double[][] elements;
+  private int length;
+  
+  // Creates empty matrix
+  public SquareMatrix() {
+  }
+  
+  // Creates SquareMatrix from an array
+  public SquareMatrix(double[][] elements) throws Exception {
+    if(elements.length != Array.getLength(elements[0])) {
+      throw new Exception("Rows and columns must have same length");
+    }
+    else {
+      this.elements = elements;
+      this.length = elements.length;
+    }
+  }
+  
+  // Returns string representation of matrix
+  public String toString() {
+    String output =  "";
+    for(int i = 0; i < this.length; i++) {
+      output += " | ";
+      for(int j = 0; j < this.length; j++) {
+        output += elements[i][j] + " | ";
+      }
+      output += "\n"; 
+      }
+    return output;
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
--- PHAS3459/src/module5/TestSquareMatrix.java
+++ PHAS3459/src/module5/TestSquareMatrix.java
@@ -4,6 +4,44 @@ public class TestSquareMatrix {
 
   public static void main(String[] args) {
     
+    SquareMatrix A = new SquareMatrix();
+    double[][] Avals = { {1, 2, 0}, {0, 2, 0}, {-2, 0, 1} };
+    try {
+      A = new SquareMatrix(Avals);
+    }
+    catch (Exception E) {
+      System.out.println(E);
+    }
+    SquareMatrix B = new SquareMatrix();
+    double[][] Bvals = { {2, 1, 0}, {0, 1, 0}, {-1, 0, 1} };
+    try {
+      B = new SquareMatrix(Bvals);
+    }
+    catch (Exception E) {
+      System.out.println(E);
+    }
+    SquareMatrix C = new SquareMatrix();
+    double[][] Cvals = { {4, 3}, {3, 2} };
+    try {
+      C = new SquareMatrix(Cvals);
+    }
+    catch (Exception E) {
+      System.out.println(E);
+    }
+    SquareMatrix D = new SquareMatrix();
+    double[][] Dvals = { {-2, 3}, {3, -4} };
+    try {
+      D = new SquareMatrix(Dvals);
+    }
+    catch (Exception E) {
+      System.out.println(E);
+    }
+    
+    System.out.println(A);
+    System.out.println(B);
+    System.out.println(C);
+    System.out.println(D);
+
 
   }
 
