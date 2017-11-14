20c64528f5f879bdca6b66f82e10c8a4aba11c79
James Williams <zcapjwi@ucl.ac.uk>
2017-11-14 16:48:56 +0000

    added constructors & parseLine method

--- PHAS3459/src/pastpaper2016/Player.java
+++ PHAS3459/src/pastpaper2016/Player.java
@@ -1,5 +1,7 @@
 package pastpaper2016;
 
+import java.util.Scanner;
+
 /*
  * Represents an MBL player and includes method to parse a line of data into Player object
  */
@@ -21,4 +23,62 @@ public class Player {
   private double onBase; // On-Base percentage: fraction of times player reached base (either from a hit,
                          // walk or hit-by-pitch)
 
+  // Empty constructor sets all numbers to 0 and Strings to ""
+  public Player() {
+    this.playerName = "";
+    this.team = "";
+    this.position = "";
+    this.games = 0;
+    this.atBats = 0;
+    this.runs = 0;
+    this.hits = 0;
+    this.doubles = 0;
+    this.triples = 0;
+    this.homeRuns = 0;
+    this.runsBattedIn = 0;
+    this.battingAverage = 0.0;
+    this.onBase = 0.0;
+  }
+
+  // Constructor sets all variables same as input
+  public Player(String playerName, String team, String position, int games, int atBats, int runs, int hits, int doubles,
+      int triples, int homeRuns, int runsBattedIn, double battingAverage, double onBase) {
+    this.playerName = playerName;
+    this.team = team;
+    this.position = position;
+    this.games = games;
+    this.atBats = atBats;
+    this.runs = runs;
+    this.hits = hits;
+    this.doubles = doubles;
+    this.triples = triples;
+    this.homeRuns = homeRuns;
+    this.runsBattedIn = runsBattedIn;
+    this.battingAverage = battingAverage;
+    this.onBase = onBase;
+  }
+
+  // Parses a tab-separated line of data about player into Player object
+  public static Player parseLine(String line) {
+    Player p = new Player();
+    Scanner s = new Scanner(line).useDelimiter("\t");
+    // Iterates while next token present
+    while (s.hasNext()) {
+      p.playerName = s.next();
+      p.team = s.next();
+      p.position = s.next();
+      p.games = s.nextInt();
+      p.atBats = s.nextInt();
+      p.runs = s.nextInt();
+      p.hits = s.nextInt();
+      p.doubles = s.nextInt();
+      p.triples = s.nextInt();
+      p.homeRuns = s.nextInt();
+      p.runsBattedIn = s.nextInt();
+      p.battingAverage = s.nextDouble();
+      p.onBase = s.nextDouble();
+    }
+    s.close();
+    return p;
+  }
 }
