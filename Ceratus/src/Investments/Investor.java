604794bbe6907b0c3ac75b784eeda2c85318f76c
James Williams <zcapjwi@ucl.ac.uk>
2017-11-19 20:52:28 +0000

    Added some variables (not yet used) and a Data (main) class

--- Ceratus/src/Investments/Investor.java
+++ Ceratus/src/Investments/Investor.java
@@ -5,5 +5,10 @@ package Investments;
  */
 
 public class Investor {
-
+  
+  private String name; // Investor's name
+  private double initialInvestment; // Initial investment in GBP
+  private double totalInvestment; // Sum of all money deposited
+  private double startDate; // Date of initial investment 
+  
 }
