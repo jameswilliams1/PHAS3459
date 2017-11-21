30e78b7960b006557bb2c944b87b1eca84c8a09a
James Williams <zcapjwi@ucl.ac.uk>
2017-11-21 16:52:20 +0000

    created LabelledDataPoint class and copied DataPoint from module5

---
+++ PHAS3459/src/module6/DataPoint.java
@@ -0,0 +1,65 @@
+package module6;
+
+import java.util.Scanner;
+
+public class DataPoint {
+  /*
+   * Represents results of measurements: x, y & error in y: ey & includes method
+   * to parse data (as string) into DataPoint object
+   */
+  private final double x; // x measurement
+  private final double y; // y measurement
+  private final double ey; // Error in y
+
+  public DataPoint() { // Sets all values to 0.0 when no arguments
+    this.x = 0.0;
+    this.y = 0.0;
+    this.ey = 0.0;
+  }
+
+  // Inputs x, y, ey
+  public DataPoint(double x, double y, double ey) {
+    this.x = x;
+    this.y = y;
+    this.ey = ey;
+  }
+
+  // Gets value of x
+  public double getX() {
+    return this.x;
+  }
+
+  // Gets value of y
+  public double getY() {
+    return this.y;
+  }
+
+  // Gets value of ey
+  public double getEy() {
+    return this.ey;
+  }
+
+  // Parses input string line (3 numbers) into DataPoint object containing x, y,
+  // ey
+  public static DataPoint parseLine(String line) {
+    Scanner s = new Scanner(line);
+    double x = 0.0;
+    double y = 0.0;
+    double ey = 0.0;
+    // Iterates while next token present
+    while (s.hasNext()) {
+      // Updates each value with next token
+      x = s.nextDouble();
+      y = s.nextDouble();
+      ey = s.nextDouble();
+    }
+    s.close();
+    // Creates DataPoint from stored values
+    return new DataPoint(x, y, ey);
+  }
+
+  // Displays DataPoint as String
+  public String toString() {
+    return "x = " + x + ", y = " + y + " +- " + ey;
+  }
+}
---
+++ PHAS3459/src/module6/LabelledDataPoint.java
@@ -0,0 +1,9 @@
+package module6;
+
+/*
+ * Extends DataPoint class to add labels
+ */
+
+public class LabelledDataPoint extends DataPoint {
+
+}
