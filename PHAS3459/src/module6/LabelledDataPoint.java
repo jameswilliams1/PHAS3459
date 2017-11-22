3e4984076ac11d86fec6851457c70700342aa1da
James Williams <zcapjwi@ucl.ac.uk>
2017-11-22 12:34:19 +0000

    Added main class (TestDataPoints) & updated parseLine to add a label in LabelledDataPoint

--- PHAS3459/src/module6/LabelledDataPoint.java
+++ PHAS3459/src/module6/LabelledDataPoint.java
@@ -1,5 +1,7 @@
 package module6;
 
+import java.util.Scanner;
+
 /*
  * Extends DataPoint class to add labels
  */
@@ -16,14 +18,40 @@ public class LabelledDataPoint extends DataPoint {
 
   // Adds input for label
   public LabelledDataPoint(double x, double y, double ey, String label) {
-      super(x, y, ey);
-      this.label = label;
-    }
+    super(x, y, ey);
+    this.label = label;
+  }
 
   // Displays DataPoint as String
   @Override
   public String toString() {
     return label + ": " + super.toString();
   }
+  
+//Parses input string line (3 numbers and label) into LabelledDataPoint object containing x, y,
+ // ey & label
+ public static DataPoint parseLine(String line) {
+   Scanner s = new Scanner(line);
+   double x = 0.0;
+   double y = 0.0;
+   double ey = 0.0;
+   String label = "";
+   // Iterates while next token present
+   while (s.hasNext()) {
+     // Updates each value with next token
+     x = s.nextDouble();
+     y = s.nextDouble();
+     ey = s.nextDouble();
+     if(s.hasNext()) {
+       label = s.next();
+     }
+   }
+   s.close();
+   if ( label != null) {
+     return new LabelledDataPoint(x, y, ey, label);
+   }
+   // Creates DataPoint from stored values
+   return new DataPoint(x, y, ey);
+ }
 
 }
