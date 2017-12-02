92d407f6e959b76bb8a38df237e0837b8902ea9a
James Williams <zcapjwi@ucl.ac.uk>
2017-12-02 21:44:13 +0000

    Added Theory, PowerLawTheory & Quadratic Theory classes with constructors + toString, tidied code in all other classes

---
+++ PHAS3459/src/module6/PowerLawTheory.java
@@ -0,0 +1,30 @@
+package module6;
+
+/*
+ * Represents function y = x^n
+ */
+
+public class PowerLawTheory implements Theory {
+  
+  private double n; // Power to raise x to
+  
+  // Constructor sets value of n
+  public PowerLawTheory(int n) {
+    this.n = n;
+  }
+
+  // Returns y = x^n
+  @Override
+  public double y(double x) {
+    return Math.pow(x, n);
+  }
+
+  // Displays function x^n as string
+  @Override
+  public String toString() {
+    return "x^" + n;
+  }
+  
+  
+
+}
