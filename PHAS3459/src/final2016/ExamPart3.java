1211f5d9b34cecb595ca7a7822e17031e7e1bf92
James Williams <zcapjwi@ucl.ac.uk>
2018-01-14 18:14:16 +0000

    Created ExamPart3 (main) + SpectralDenisty class (not finished)

---
+++ PHAS3459/src/final2016/ExamPart3.java
@@ -0,0 +1,9 @@
+package final2016;
+
+public class ExamPart3 {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ PHAS3459/src/final2016/SpectralDensity.java
@@ -0,0 +1,15 @@
+package final2016;
+
+/**
+ * Determines the spectral density of an Audio object
+ * @author James
+ *
+ */
+public class SpectralDensity implements Classify {
+
+  @Override
+  public String classify(Audio a) {
+    
+  }
+
+}
