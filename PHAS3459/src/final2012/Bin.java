09e2333f2023d09a2edf41702375d6b6bbd8c67d
James Williams <zcapjwi@ucl.ac.uk>
2018-01-16 21:12:51 +0000

    moved parseLine to new Data class

--- PHAS3459/src/final2012/Bin.java
+++ PHAS3459/src/final2012/Bin.java
@@ -20,23 +20,4 @@ public class Bin {
     this.count = count;
   }
 
-  /**
-   * Parses a String line containing bin bounds and count into a Bin object
-   * 
-   * @param line
-   * @return
-   */
-  public Bin parseLine(String line) {
-    Scanner s = new Scanner(line);
-    double minE = 0.0;
-    double maxE = 0.0;
-    double count = 0.0;
-    if (s.hasNext()) {
-      minE = s.nextDouble();
-      maxE = s.nextDouble();
-      count = s.nextDouble();
-    }
-    s.close();
-    return new Bin(minE, maxE, count);
-  }
 }
--- PHAS3459/src/final2012/Events.java
+++ PHAS3459/src/final2012/Events.java
@@ -1,5 +1,7 @@
 package final2012;
 
+import java.util.ArrayList;
+
 /**
  * Represents list of events recorded/predicted for the ATLAS experiment
  * 
@@ -7,5 +9,7 @@ package final2012;
  *
  */
 public class Events {
+
+  private ArrayList<Bin> data; // list of events/predictions Bins
 
 }
