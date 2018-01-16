f770400cc0ec6480dd0da35d5701a6995e631250
James Williams <zcapjwi@ucl.ac.uk>
2018-01-16 22:37:55 +0000

    working dataFromURL (to arrays)

--- PHAS3459/src/final2012/Bin.java
+++ PHAS3459/src/final2012/Bin.java
@@ -1,7 +1,5 @@
 package final2012;
 
-import java.util.Scanner;
-
 /**
  * Represents an energy bin between 2 values for an ATLAS experiment
  * 
@@ -13,6 +11,18 @@ public class Bin {
   private double minE; // bin lower bound
   private double maxE; // bin upper bound
   private double count; // number of readings in bin
+
+  public double getMinE() {
+    return this.minE;
+  }
+
+  public double getMaxE() {
+    return this.maxE;
+  }
+
+  public double getCount() {
+    return this.count;
+  }
 
   public Bin(double minE, double maxE, double count) {
     this.minE = minE;
@@ -26,6 +36,14 @@ public class Bin {
   @Override
   public String toString() {
     return "Bin [minE=" + minE + ", maxE=" + maxE + ", count=" + count + "]";
+  }
+
+  public boolean inRange(double lower, double upper) {
+    if (this.minE >= lower && this.maxE < upper) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
--- PHAS3459/src/final2012/Data.java
+++ PHAS3459/src/final2012/Data.java
@@ -1,5 +1,9 @@
 package final2012;
 
+/**
+ * Tools for analysing data using Bin objects
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,8 +11,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-import final2016.Audio;
 
 public class Data {
 
@@ -31,15 +33,22 @@ public class Data {
     s.close();
     return new Bin(minE, maxE, count);
   }
-  
-  public static ArrayList<Bin> dataFromURL(String url) throws IOException {
+
+  /**
+   * Gets expected data from a url, returns list of Bin objects
+   * 
+   * @param url
+   * @return
+   * @throws IOException
+   */
+  public static ArrayList<Bin> theoryFromURL(String url) throws IOException {
     ArrayList<Bin> data = new ArrayList<Bin>();
     URL u = new URL(url);
     InputStream is = u.openStream();
     InputStreamReader isr = new InputStreamReader(is);
     BufferedReader br = new BufferedReader(isr);
     String line = "";
-    br.readLine(); //skips headers (1 line)
+    br.readLine(); // skips headers (1 line)
     while ((line = br.readLine()) != null) {
       Bin b = parseLine(line);
       data.add(b);
@@ -47,20 +56,74 @@ public class Data {
     return data;
   }
 
+  /**
+   * Counts the number of events in a specified range (min-max) for list of Bin
+   * (bins)
+   * 
+   * @param bins
+   * @param min
+   * @param max
+   * @return
+   */
+  public static double countEvents(ArrayList<Bin> bins, double min, double max) {
+    double count = 0.0;
+    for (Bin bin : bins) {
+      if (bin.inRange(min, max)) {
+        count += bin.getCount();
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Returns list  of event energies for a given channel (String)
+   * @param ID
+   * @param url
+   * @return
+   * @throws IOException
+   */
+  public static ArrayList<Double> dataFromURL(String ID, String url) throws IOException {
+    URL u = new URL(url);
+    InputStream is = u.openStream();
+    InputStreamReader isr = new InputStreamReader(is);
+    BufferedReader br = new BufferedReader(isr);
+    String line = "";
+    ArrayList<Double> data = new ArrayList<Double>();
+    br.readLine(); // skips headers (1 line)
+    double thisValue = 0.0;
+    String thisID = "";
+    while ((line = br.readLine()) != null) { //iterates over each line
+      Scanner s = new Scanner(line);
+      if ( s.hasNext()) {
+        thisID = s.next(); //updates thisID to next token
+        if ( thisID.equals(ID)) { //checks if thisID is the same as required ID (input)
+          thisValue = s.nextDouble();
+          data.add(thisValue); //adds value to list if ID matches
+        }
+      }
+      s.close();
+    }
+    return data;
+  }
+
   public static void main(String[] args) {
-    
+
     ArrayList<Bin> backgroundGG = new ArrayList<Bin>();
     ArrayList<Bin> backgroundZZ = new ArrayList<Bin>();
+    ArrayList<Double> dataGG = new ArrayList<Double>();
+    ArrayList<Double> dataZZ = new ArrayList<Double>();
     try {
-      backgroundGG = dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2012-13/backgroundGG.txt");
-      backgroundZZ = dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2012-13/backgroundZZ.txt");
+      backgroundGG = theoryFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2012-13/backgroundGG.txt");
+      backgroundZZ = theoryFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2012-13/backgroundZZ.txt");
+      dataGG = dataFromURL("GG", "http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2012-13/higgsData.txt");
+      dataZZ = dataFromURL("ZZ", "http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2012-13/higgsData.txt");
     } catch (IOException e) {
       System.out.println(e);
     }
-    
-    System.out.println(backgroundGG);
-    System.out.println(backgroundZZ);
-    
+    //System.out.println("Expected background events between 120-140GeV (GG): " + countEvents(backgroundGG, 120, 140));
+    //System.out.println("Expected background events between 120-140GeV (ZZ): " + countEvents(backgroundZZ, 120, 140));
+    System.out.println(dataGG.size());
+    System.out.println(dataZZ.size());
 
   }
 
