47d8ecfff0da1322bfa611b8094e4f537ad7e0a5
James Williams <zcapjwi@ucl.ac.uk>
2018-01-16 14:26:00 +0000

    created findTime interface

---
+++ PHAS3459/src/final2015/findTime.java
@@ -0,0 +1,12 @@
+package final2015;
+
+/**
+ * Interface to find the time taken by a particle tpo reach a detector
+ * @author James
+ *
+ */
+public interface findTime {
+  
+  public int FindT();
+
+}
